e6b5b33983d4898bf9fb4ad33c99e52b5907b086
Dan Birmingham <dgbirm@gmail.com>
2020-07-22 01:52:03 -0400

    still trying to integrate react

--- src/main/js/src/App.js
+++ src/main/js/src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+'use strict';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const React = require('react');
+const ReactDOM = require('react-dom');
+const client = require('./client');
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+)
+
+///// App ///////////////////////////////////////
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+        this.state = {
+            employees: [],
+            departments: []
+        };
+	}
+
+	componentDidMount() {
+		client({method: 'GET', path: '/api/employees'}).done(response => {
+            this.setState({employees: response.entity._embedded.employees});
+        })
+        client({method: 'GET', path: '/api/departments'}).done(response => {
+            this.setState({departments: response.entity._embedded.departments});
+        });
+	}
+
+	render() {
+		return (<li>
+                    <EmployeeList employees={this.state.employees}/>
+                    <DepartmentList departments={this.state.departments}/>
+                </li>
+		)
+	}
 }
 
-export default App;
+///// EmployeeList //////////////////////////////
+
+class EmployeeList extends React.Component{
+	render() {
+		const employees = this.props.employees.map(employee =>
+			<Employee key={employee._links.self.href} employee={employee}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+                        <th>EMP_ID</th>
+						<th>fullName</th>
+						<th>dep</th>
+						<th>yearlySalary</th>
+                        <th>jobTitle</th>
+					</tr>
+					{employees}
+				</tbody>
+			</table>
+		)
+	}
+}
+
+///// DepartmentList ////////////////////////////
+class DepartmentList extends React.Component{
+	render() {
+		const departments = this.props.departments.map(department =>
+			<Department key={department._links.self.href} department={department}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+                        <th>DEP_ID</th>
+						<th>depName</th>
+						<th>depHead</th>
+					</tr>
+					{departments}
+				</tbody>
+			</table>
+		)
+	}
+}
+
+///// Employee //////////////////////////////////
+
+class Employee extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.employee.EMP_ID}</td>
+				<td>{this.props.employee.fullname}</td>
+				<td>{this.props.employee.dep}</td>
+                <td>{this.props.employee.yearlySalary}</td>
+                <td>{this.props.employee.jobTitle}</td>
+			</tr>
+		)
+	}
+}
+
+///// Department ////////////////////////////////
+
+class Department extends React.Component{
+    render() {
+        return (
+            <tr>
+                <td>{this.props.department.DEP_ID}</td>
+                <td>{this.props.department.depName}</td>
+                <td>{this.props.department.depHead}</td>
+            </tr>
+        )
+    }
+}
